87e3f18dbc761c3e515e9fd44a3067a4c027cc48
LeninCar <lenin.carabali@correounivalle.edu.co>
2023-09-03 17:02:54 -0500

    First Commit, Box

---
+++ src/Experience.jsx
@@ -0,0 +1,24 @@
+import { OrbitControls } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Experience = ({title, info}) => { //usando destructuring
+    const boxRef = useRef();
+    useFrame((state, delta) => {
+        boxRef.current.rotation.x += 1 * delta;
+    })
+
+    return ( 
+    <>
+        <OrbitControls makeDefault />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={2} />
+
+        <mesh ref={boxRef}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="#935116" />
+        </mesh>
+    </>
+    );
+}
+export default Experience;
