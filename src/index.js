9a19d9a364510bb01f52b5c541f9602d92944c1d
LeninCar <lenin.carabali@correounivalle.edu.co>
2023-09-05 18:58:43 -0500

    Animación al titulo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App";
+import gsap from 'gsap'
 
 const root = createRoot(document.getElementById("root"));
+
+const text = document.getElementsByClassName('title');
+const range = 30;
+
+function updateShadow(event) {
+    const x = Math.round(event.pageX * range / window.innerWidth) - range / 2;
+    const y = Math.round(event.pageY * range / window.innerHeight) - range / 2;
+    gsap.to(text, {
+      '--x': x,
+      '--y': y,
+    });
+  }
+  
+  document.body.addEventListener('mousemove', updateShadow);
 
 root.render(
     <App />
